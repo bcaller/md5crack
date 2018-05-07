ff3b324354664e46940f36c30a368e52ae0edf6c
bcaller <bcaller@users.noreply.github.com>
2018-05-07 16:10:31 +0100

    Compare against [u8] rather than string (faster)
    One example takes 29.2s rather than 46.1s to complete.
    
    I need to now read up on what Result is - it looks like the Haskell
    Maybe.

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,23 @@ fn main() {
     if target.chars().count() != 32 {
         panic!("Hash should be 32 chars long")
     }
+    let mut target_bytes = [0; 16];
+    for i in 0..16 {
+        target_bytes[i] = u8::from_str_radix(&target[2*i..(2*i+2)], 16).unwrap_or(0);
+    }
     println!("Searching for {}", target);
 
     let mut md5 = Md5::new();
     for i in (0..100000000).rev() {
         let key = format!("{:08}", i);
         md5.input_str(&key);
-        let output = md5.result_str();
+        let mut output_bytes = [0; 16];
+        md5.result(&mut output_bytes);
         if i % 1000000 == 0 {
-            println!("{} - {}", key, output)
+            println!("{} - {}", key, md5.result_str())
         }
-        if output.eq(target) {
-            println!("{} - {}", key, output);
+        if output_bytes == target_bytes {
+            println!("{} - {}", key, md5.result_str());
             break
         }
         md5.reset()
